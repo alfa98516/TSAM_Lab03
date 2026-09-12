e54e2ca2a0616bc734aa1127a8fadb4117bb6587
Kornelius-ux <hlynurh24@ru.is>
2026-09-12 14:35:26 +0000

    started on solve_puzzle_a()

--- puzzlesolver.cpp
+++ puzzlesolver.cpp
@@ -177,7 +177,16 @@ namespace {
         std::array<std::string, 4> problem_ports = assign_problems_to_port();
     }
 
-void solve_puzzle_a() {}
+int secret_sigil;
+
+void solve_puzzle_a() {
+
+    //fun fact, this is a prime :O
+    const int secret_number = 2<<31 - 1; 
+    // More formally, this is the eight Mersenne prime,
+    // Where Mersenne prime is the collection of primes which satisfy this condition;
+    // 2^n - 1
+}
 void solve_puzzle_b() {}
 void solve_puzzle_c() {}
 void solve_puzzle_d() {}
